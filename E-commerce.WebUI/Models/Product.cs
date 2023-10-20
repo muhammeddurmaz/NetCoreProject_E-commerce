012c50119b87e388f37ba765bc5ba83c7761b359
muhammeddurmaz <muhammed.durmaz@derinarge.com>
2023-10-20 09:06:41 +0300

    product model added

---
+++ E-commerce.WebUI/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace E_commerce.WebUI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+    }
+}
