590a7d628b63975626f679601df58c4fd6aaf00d
muhammeddurmaz <muhammed.durmaz@derinarge.com>
2023-10-21 22:00:49 +0300

    products added

--- E-commerce.WebUI/Controllers/ProductController.cs
+++ E-commerce.WebUI/Controllers/ProductController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_commerce.WebUI.Models;
+using E_commerce.WebUI.Service;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace E_commerce.WebUI.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly ProductService _productService;
+
+        public ProductController(ProductService productService)
+        {
+            this._productService = productService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Product> products = _productService.getAllProduct();
+            return View(products);
         }
         public IActionResult Details()
         {
